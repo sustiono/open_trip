f91e101c39bf2e026645f4e2fc7ff9bdb47e8d76
Sustiono <sustiono19@gmail.com>
2020-03-20 20:15:12 +0700

    Add relation user to trips

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many    :schedules
+  has_many    :open_trips, class_name: 'BookedTrip', foreign_key: :user_id
+  has_many    :trips
   belongs_to  :role
 
   validates :name, :address, :phone_number, :role_id, presence: true
