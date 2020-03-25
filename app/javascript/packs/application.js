85d48a7de0cf9618ae7a118e38a199f15049d5d2
Sustiono <sustiono19@gmail.com>
2020-03-25 18:05:32 +0700

    Bootstrap integration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+import '../stylesheets/application'
+import './bootstrap_custom.js'
