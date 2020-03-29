1d39e7725e562671dd9eb15206690cf1476dfcb9
Sustiono <sustiono19@gmail.com>
2020-03-29 22:25:35 +0700

    Add sign in button & restrict home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
+  before_action :restrict_this_page
+
   def index
     @trips = Trip.all
+  end
+
+  private
+  def restrict_this_page
+    if current_user&.is_travel_agent?
+      return redirect_to trips_path
+    elsif current_user&.is_traveler?
+      return redirect_to open_trips_path
+    end
   end
 end
